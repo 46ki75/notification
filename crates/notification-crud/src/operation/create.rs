153bc476e8c8bcd877690de0fe3b4b903b516810
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-04-08 19:09:25 +0000

    feat(notification-crud): add create operation and update types

---
+++ crates/notification-crud/src/operation/create.rs
@@ -0,0 +1,71 @@
+use serde::{Deserialize, Serialize};
+#[derive(Serialize, Deserialize, Debug)]
+pub struct CreateParameter {
+    pub title: String,
+    pub details: Option<String>,
+    pub status: Option<crate::r#type::Status>,
+    pub severity: Option<crate::r#type::Severity>,
+    pub url: Option<String>,
+}
+
+pub async fn create(
+    parameter: CreateParameter,
+) -> Result<crate::r#type::Notification, lambda_runtime::Error> {
+    let table_name = std::env::var("TABLE_NAME")?;
+
+    let aws_sdk_config = aws_config::load_defaults(aws_config::BehaviorVersion::latest()).await;
+
+    let dynamodb_client = std::sync::Arc::new(aws_sdk_dynamodb::Client::new(&aws_sdk_config));
+
+    let pk = uuid::Uuid::new_v4().to_string();
+
+    let notification = crate::r#type::Notification {
+        pk,
+        title: parameter.title,
+        details: parameter.details,
+        status: parameter.status.unwrap_or_default(),
+        severity: parameter.severity.unwrap_or_default(),
+        notified_at: chrono::Utc::now().to_rfc3339(),
+        url: parameter.url,
+    };
+
+    let request = dynamodb_client
+        .put_item()
+        .table_name(table_name)
+        .item(
+            "PK",
+            aws_sdk_dynamodb::types::AttributeValue::S(String::from(&notification.pk)),
+        )
+        .item(
+            "title",
+            aws_sdk_dynamodb::types::AttributeValue::S(String::from(&notification.title)),
+        )
+        .item(
+            "details",
+            aws_sdk_dynamodb::types::AttributeValue::S(String::from(
+                &notification.details.clone().unwrap_or_default(),
+            )),
+        )
+        .item(
+            "status",
+            aws_sdk_dynamodb::types::AttributeValue::S(String::from(
+                &notification.status.to_string(),
+            )),
+        )
+        .item(
+            "severity",
+            aws_sdk_dynamodb::types::AttributeValue::S(String::from(
+                &notification.severity.to_string(),
+            )),
+        )
+        .item(
+            "url",
+            aws_sdk_dynamodb::types::AttributeValue::S(String::from(
+                notification.url.clone().unwrap_or_default().to_string(),
+            )),
+        );
+
+    let _response = request.send().await?;
+
+    Ok(notification)
+}
--- crates/notification-crud/src/type.rs
+++ crates/notification-crud/src/type.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug, Default)]
+#[derive(Serialize, Deserialize, Debug, Default, Clone, Copy)]
 #[serde(rename = "SCREAMING_SNAKE_CASE")]
 pub enum Status {
     #[default]
@@ -10,7 +10,19 @@ pub enum Status {
     Suppressed,
 }
 
-#[derive(Serialize, Deserialize, Debug, Default)]
+impl std::fmt::Display for Status {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let status_str = match self {
+            Status::New => "NEW",
+            Status::Open => "OPEN",
+            Status::Resolved => "RESOLVED",
+            Status::Suppressed => "SUPPRESSED",
+        };
+        write!(f, "{}", status_str)
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Default, Clone, Copy)]
 #[serde(rename = "SCREAMING_SNAKE_CASE")]
 pub enum Severity {
     #[default]
@@ -19,7 +31,18 @@ pub enum Severity {
     Error,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+impl std::fmt::Display for Severity {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let severity_str = match self {
+            Severity::Info => "INFO",
+            Severity::Warn => "WARN",
+            Severity::Error => "ERROR",
+        };
+        write!(f, "{}", severity_str)
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Notification {
     #[serde(rename = "PK")]
     pub pk: String,
@@ -31,41 +54,41 @@ pub struct Notification {
     pub url: Option<String>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-pub enum Input {
-    Create(CreateParameter),
-    Read(ReadParameter),
-    Update(UpdateParameter),
-    Delete(DeleteParameter),
-}
+// #[derive(Serialize, Deserialize, Debug)]
+// pub enum Input {
+//     Create(CreateParameter),
+//     Read(ReadParameter),
+//     Update(UpdateParameter),
+//     Delete(DeleteParameter),
+// }
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct CreateParameter {
-    pub title: String,
-    pub details: Option<String>,
-    pub status: Option<Status>,
-    pub severity: Option<Severity>,
-    pub url: Option<String>,
-}
+// #[derive(Serialize, Deserialize, Debug)]
+// pub struct CreateParameter {
+//     pub title: String,
+//     pub details: Option<String>,
+//     pub status: Option<Status>,
+//     pub severity: Option<Severity>,
+//     pub url: Option<String>,
+// }
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct ReadParameter {
-    pub status_query: Vec<Status>,
-}
+// #[derive(Serialize, Deserialize, Debug)]
+// pub struct ReadParameter {
+//     pub status_query: Vec<Status>,
+// }
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct UpdateParameter {
-    #[serde(rename = "PK")]
-    pub pk: String,
-    pub title: Option<String>,
-    pub details: Option<String>,
-    pub status: Option<Status>,
-    pub severity: Option<Severity>,
-    pub url: Option<String>,
-}
+// #[derive(Serialize, Deserialize, Debug)]
+// pub struct UpdateParameter {
+//     #[serde(rename = "PK")]
+//     pub pk: String,
+//     pub title: Option<String>,
+//     pub details: Option<String>,
+//     pub status: Option<Status>,
+//     pub severity: Option<Severity>,
+//     pub url: Option<String>,
+// }
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct DeleteParameter {
-    #[serde(rename = "PK")]
-    pub pk: String,
-}
+// #[derive(Serialize, Deserialize, Debug)]
+// pub struct DeleteParameter {
+//     #[serde(rename = "PK")]
+//     pub pk: String,
+// }
