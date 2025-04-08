f859fb21ed30a05302c2a1196653385e5b22018e
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-04-08 16:23:54 +0000

    chore: update repository name in locals.tf and configure S3 backend in provider.tf

--- terraform/github/provider.tf
+++ terraform/github/provider.tf
@@ -5,4 +5,13 @@ terraform {
       version = "~> 6.0"
     }
   }
+  
+  backend "s3" {
+    bucket               = "shared-46ki75-notification-s3-bucket-terraform-tfstate"
+    workspace_key_prefix = "github"
+    key                  = "terraform.tfstate"
+    region               = "ap-northeast-1"
+    encrypt              = true
+    use_lockfile         = true
+  }
 }
